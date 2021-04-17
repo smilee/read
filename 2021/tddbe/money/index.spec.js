3762a041365ab298bcf0d8bacf476383ddbaf5b8
Sena <ideas007@naver.com>
2021-04-17 23:55:09 +0900

    test(Franc): test Franc constructor

--- 2021/tddbe/money/index.spec.js
+++ 2021/tddbe/money/index.spec.js
@@ -1,4 +1,4 @@
-import { Dollar } from './index';
+import { Dollar, Franc } from './index';
 
 describe('Dollar', () => {
   context('when it is created', () => {
@@ -23,6 +23,35 @@ describe('Dollar', () => {
     it('returns whether they are equal', () => {
       const five = new Dollar(5);
       const six = new Dollar(6);
+
+      expect(five.equals(six)).toBe(false);
+    });
+  });
+});
+
+describe('Franc', () => {
+  context('when it is created', () => {
+    it('has an amount', () => {
+      const franc = new Franc(5);
+
+      expect(franc.amount).toBe(5);
+    });
+  });
+
+  context('when it is multiplied by a multiplier', () => {
+    it('returns the multiple', () => {
+      const franc = new Franc(5);
+
+      franc.times(2);
+
+      expect(franc.amount).toBe(10);
+    });
+  });
+
+  context('when francs of differing amounts are compared with each other', () => {
+    it('returns whether they are equal', () => {
+      const five = new Franc(5);
+      const six = new Franc(6);
 
       expect(five.equals(six)).toBe(false);
     });
