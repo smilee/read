03b7510b97709c1d447fc74c55567324cfd65491
Sena <ideas007@naver.com>
2021-04-17 23:12:28 +0900

    test(Dollar): test the times method

--- 2021/tddbe/money/index.spec.js
+++ 2021/tddbe/money/index.spec.js
@@ -6,4 +6,11 @@ describe('Dollar', () => {
       expect(new Dollar(5).amount).toBe(5);
     });
   });
+  context('when it is multiplied by a multiplier', () => {
+    it('returns the multiple', () => {
+      const dollar = new Dollar(5);
+      dollar.times(2);
+      expect(dollar.amount).toBe(10);
+    });
+  });
 });
