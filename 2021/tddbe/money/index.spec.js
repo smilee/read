d0a5c562e1441abbf598431c7f68b775a1f628cd
Sena <ideas007@naver.com>
2021-04-17 23:48:34 +0900

    test(Dollar): test equals method

--- 2021/tddbe/money/index.spec.js
+++ 2021/tddbe/money/index.spec.js
@@ -3,14 +3,28 @@ import { Dollar } from './index';
 describe('Dollar', () => {
   context('when it is created', () => {
     it('has an amount', () => {
-      expect(new Dollar(5).amount).toBe(5);
+      const dollar = new Dollar(5);
+
+      expect(dollar.amount).toBe(5);
     });
   });
+
   context('when it is multiplied by a multiplier', () => {
     it('returns the multiple', () => {
       const dollar = new Dollar(5);
+
       dollar.times(2);
+
       expect(dollar.amount).toBe(10);
+    });
+  });
+
+  context('when dollars of differing amounts are compared with each other', () => {
+    it('returns whether they are equal', () => {
+      const five = new Dollar(5);
+      const six = new Dollar(6);
+
+      expect(five.equals(six)).toBe(false);
     });
   });
 });
