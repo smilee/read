75a2b21accfe211138d0a577e6711b5d5908a9ce
Sena <ideas007@naver.com>
2021-04-19 22:19:26 +0900

    feat(Wallet): add Wallet constructor

--- 2021/tddbe/money/index.js
+++ 2021/tddbe/money/index.js
@@ -14,12 +14,42 @@ function Money(amount, currency) {
 function Bank() {
   this.rates = new Map();
   this.rates.set(JSON.stringify(['CHF', 'USD']), 2);
+  this.rates.set(JSON.stringify(['USD', 'USD']), 1);
 
   this.reduce = (subject, target) => {
     const rate = this.rates.get(JSON.stringify([subject.currency, target]));
 
-    return new Money(subject.amount / rate, target);
+    return new Money(Number(subject.amount / rate), target);
   };
 }
 
-export { Money, Bank };
+function Wallet() {
+  this.reserve = {};
+
+  this.add = (money) => {
+    const augend = money.currency in this.reserve
+      ? this.reserve[money.currency].amount : 0;
+    const addend = money.amount;
+
+    this.reserve[money.currency] = new Money(augend + addend, money.currency);
+  };
+
+  this.reduce = (bank, target) => {
+    const amount = Object.values(this.reserve).reduce((sum, value) => {
+      const toTarget = bank.reduce(new Money(value.amount, value.currency), target).amount;
+      return sum + toTarget;
+    }, 0);
+    return new Money(amount, target);
+  };
+}
+
+const wallet = new Wallet();
+const dollar = new Money(5, 'USD');
+const franc = new Money(4, 'CHF');
+const bank = new Bank();
+wallet.add(dollar);
+wallet.add(franc);
+const inDollars = wallet.reduce(bank, 'USD');
+console.log(`I have ${inDollars.amount} ${inDollars.currency}.`);
+
+export { Money, Bank, Wallet };
